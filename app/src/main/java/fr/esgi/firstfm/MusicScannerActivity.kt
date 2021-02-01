4d02e02027f3330241e58b57c1f52b093a6782cf
Antonin Texier <antonin.texier@gmail.com>
2021-02-01 09:08:02 +0100

    Update main menu and add colors app theme.

--- app/src/main/java/fr/esgi/firstfm/MusicScannerActivity.kt
+++ app/src/main/java/fr/esgi/firstfm/MusicScannerActivity.kt
@@ -10,7 +10,6 @@ import android.net.NetworkCapabilities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Toast
